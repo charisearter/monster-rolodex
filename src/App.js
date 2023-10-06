f478fcf330e19dd972b13334491a047d7457461e
Reese <arter.charise@gmail.com>
2023-10-05 17:19:03 -0700

    monster rolodex initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [],
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((res) => res.json())
+			.then((monsterData) => {
+				this.setState(
+					() => {
+						return { monsters: monsterData };
+					},
+					() => {
+						console.log('Data: ', this.state.monsters);
+					}
+				);
+			});
+	}
+
+	render() {
+		//destructure from state
+		const { monsters } = this.state;
+		return (
+			<div className='App'>
+				<h1>Hello</h1>
+				{/* Map over array to display names */}
+				{monsters.map((monster) => {
+					return <h2 key={monster.id}> {monster.name} </h2>;
+				})}
+			</div>
+		);
+	}
 }
 
 export default App;
