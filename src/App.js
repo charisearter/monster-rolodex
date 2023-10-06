a52a9103ab8ef9701a7827e60169502f5ac80844
Reese <arter.charise@gmail.com>
2023-10-05 18:13:51 -0700

    added filter functionality

--- src/App.js
+++ src/App.js
@@ -13,27 +13,40 @@ class App extends Component {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then((res) => res.json())
 			.then((monsters) => {
-				this.setState(
-					() => {
-						return { monsters };
-					},
-					() => {
-						console.log(this.state);
-					}
-				);
+				this.setState(() => {
+					return { monsters };
+				});
 			});
 	}
 
 	render() {
 		// destructuring from state
 		const { monsters, searchField } = this.state;
+
+		// filter functionality
+		const filteredMonsters = monsters.filter((monster) => {
+			return monster.name.toLowerCase().includes(searchField);
+		});
+
+		
 		return (
 			<div className='container'>
 				<h1 className='title'>Monster Rolodex</h1>
 				{/* Search Field */}
+				<input
+					type='text'
+					className='search-box'
+					placeholder='Search Monsters'
+					onChange={(e) => {
+						const searchField = e.target.value.toLowerCase();
 
+						this.setState(() => {
+							return { searchField };
+						});
+					}}
+				/>
 				{/* List of Monsters */}
-				{monsters.map((monster) => {
+				{filteredMonsters.map((monster) => {
 					return <h2 key={monster.id}>{monster.name} </h2>;
 				})}
 			</div>
