1538e2a0e2cbaae914072b6b20fd15f87635bf98
Reese <arter.charise@gmail.com>
2023-10-05 18:57:19 -0700

    destructured props and methods in components

--- src/components/cardList/cardList.component.jsx
+++ src/components/cardList/cardList.component.jsx
@@ -2,10 +2,11 @@ import { Component } from 'react';
 
 class CardList extends Component {
 	render() {
+		const { monsters } = this.props;
 		return (
 			<div>
 				{/* List of Monsters - not worried about filtered monsters so just named it monsters but filtered monsters array is passed as props */}
-				{this.props.monsters.map((monster) => {
+				{monsters.map((monster) => {
 					return <h2 key={monster.id}>{monster.name} </h2>;
 				})}
 			</div>
--- src/components/searchBox/searchBox.component.jsx
+++ src/components/searchBox/searchBox.component.jsx
@@ -2,13 +2,14 @@ import { Component } from 'react';
 
 class SearchBox extends Component {
 	render() {
+		const { onSearchChange } = this.props;
 		return (
 			<div className='search-box'>
 				<input
 					type='text'
 					className='search-box'
 					placeholder='Search Monsters'
-					onChange={this.props.onSearchChange}
+					onChange={onSearchChange}
 				/>
 			</div>
 		);
